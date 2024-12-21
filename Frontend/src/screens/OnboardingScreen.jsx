a2e6983a7e0275d0262da1e31160bcd77edeed73
koraydinnc <koraydincc@icloud.com>
2024-12-21 15:43:16 +0300

    OnBoarding

--- Frontend/src/screens/OnboardingScreen.jsx
+++ Frontend/src/screens/OnboardingScreen.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 import Onboarding from 'react-native-onboarding-swiper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image, ScrollView } from 'react-native';
 
 const OnboardingScreen = ({ onComplete }) => {
   return (
-    <Onboarding
-      pages={[
-        {
-          backgroundColor: '#fff',
-          title: "Hoş Geldiniz",
-          subtitle: "Uygulamayı daha iyi kullanabilmeniz için rehberlik sağlıyoruz.",
-        },
-        {
-          backgroundColor: '#fff',
-          title: "Özellikler",
-          subtitle: "Görevlerinizi takip edebilir, tamamlanan ve devam eden görevleri görebilirsiniz.",
-        },
-        {
-          backgroundColor: '#fff',
-          title: "Başlayalım",
-          subtitle: "Artık hazırsınız! Başla butonuna basarak uygulamaya giriş yapabilirsiniz.",
-        },
-      ]}
-      onDone={onComplete} 
-      onSkip={onComplete} 
-      nextLabel="Sonraki"
-      skipLabel="Geç"
-      bottomBarColor="#fff"
-    />
+    <SafeAreaView style={{ flex: 1 }}>
+      <Onboarding
+        pages={[
+          {
+            image: <Image source={require('../../assets/favicon.png')} style={{ width: 100, height: 100 }} />,
+            backgroundColor: '#fff',
+            title: "Hoş Geldiniz",
+            subtitle: "Uygulamayı daha iyi kullanabilmeniz için rehberlik sağlıyoruz.",
+          },
+          {
+            image: <Image source={require('../../assets/favicon.png')} style={{ width: 100, height: 100 }} />,
+            backgroundColor: '#fff',
+            title: "Özellikler",
+            subtitle: "Görevlerinizi takip edebilir, tamamlanan ve devam eden görevleri görebilirsiniz.",
+          },
+          {
+            image: <Image source={require('../../assets/favicon.png')} style={{ width: 100, height: 100 }} />,
+            backgroundColor: '#fff',
+            title: "Başlayalım",
+            subtitle: "Artık hazırsınız! Başla butonuna basarak uygulamaya giriş yapabilirsiniz.",
+          },
+        ]}
+        onDone={onComplete}
+        onSkip={onComplete}
+        nextLabel="Sonraki"
+        skipLabel="Geç"
+        bottomBarColor="#fff"
+      />
+
+      </SafeAreaView>
   );
 };
 
